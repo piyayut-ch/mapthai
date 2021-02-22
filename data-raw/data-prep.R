0b41e69f35b9429b32b07fc28f973160f0b46f0b
Piyayut <piyayut.ch@gmail.com>
2021-02-22 16:15:23 +0700

    update filename

--- data-raw/data-prep.R
+++ data-raw/data-prep.R
@@ -7,9 +7,9 @@ xfun::pkg_attach2(pkgs)
 setwd(here())
 
 # read raw geojson files
-th1_sim <- st_read("data-raw/geojson/th_adm1.geojson") %>% clean_names()
-th2_sim <- st_read("data-raw/geojson/th_adm2.geojson") %>% clean_names()
-th3_sim <- st_read("data-raw/geojson/th_adm3.geojson") %>% clean_names()
+tha1_sim <- st_read("data-raw/geojson/th_adm1.geojson") %>% clean_names()
+tha2_sim <- st_read("data-raw/geojson/th_adm2.geojson") %>% clean_names()
+tha3_sim <- st_read("data-raw/geojson/th_adm3.geojson") %>% clean_names()
 # worldsub_sim <- st_read("data-raw/geojson/worldsub.geojson") %>% clean_names()
 
 # create a new sf object by aggregating up from sub district
@@ -17,25 +17,25 @@ adm3 <- c("adm3_pcode", "adm3_en", "adm3_th")
 adm2 <- c("adm2_pcode", "adm2_en", "adm2_th")
 adm1 <- c("adm1_pcode", "adm1_en", "adm1_th")
 
-th3 <- th3_sim %>%
+tha3 <- tha3_sim %>%
   select( all_of(c(adm3, adm2, adm1)) )
 
-th2 <- th3 %>%
+tha2 <- tha3 %>%
   group_by( across(c(adm2, adm1)) ) %>%
   summarise(
     geometry = st_union(geometry)
   ) %>% ungroup()
 
-th1 <- th2 %>%
+tha1 <- tha2 %>%
   group_by( across(adm1) ) %>%
   summarise(
     geometry = st_union(geometry)
   ) %>% ungroup()
 
 # generate
-ref_th <- th3 %>%
+ref_tha <- tha3 %>%
   st_drop_geometry()
 
-ref_th %>% write_xlsx("data-raw/ref/ref_th.xlsx")
+ref_tha %>% write_xlsx("data-raw/ref/ref_tha.xlsx")
 
-usethis::use_data(th3, th2, th1, ref_th, overwrite = TRUE)
+usethis::use_data(tha3, tha2, tha1, ref_tha, overwrite = TRUE)
